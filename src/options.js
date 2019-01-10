a1d47ca336407935def76ab0d8445088e6414d81
Kevin Deisz <kevin.deisz@gmail.com>
2019-01-10 11:35:24 -0500

    Further bikeshedding options

--- src/options.js
+++ src/options.js
@@ -5,12 +5,37 @@ const canRaiseStat = (originalStats, currentStats, nextStat) => {
   const compareStat = (valueA, valueB) => (valueA > valueB ? 1 : -1);
 
   return Object.keys(originalStats).every(
-    statName =>
+    statName => (
       originalStats[statName] === originalStats[nextStat] ||
       compareStat(originalStats[statName], originalStats[nextStat]) ===
         compareStat(currentStats[statName], currentStats[nextStat] + 1)
+    )
   );
 };
+
+const raiseRandomStat = (originalStats, currentStats) => {
+  const stats = Object.keys(originalStats);
+  const nextStat = stats[Math.floor(Math.random() * stats.length)];
+
+  if (canRaiseStat(originalStats, currentStats, nextStat)) {
+    return nextStat;
+  }
+  return raiseRandomStat(originalStats, currentStats);
+};
+
+const raiseEvenStat = (originalStats, currentStats) => (
+  Object.keys(originalStats).reduce((current, stat) => {
+    const delta = currentStats[stat] - originalStats[stat];
+    if (
+      (current === null ||
+        delta < currentStats[current] - originalStats[current]) &&
+      canRaiseStat(originalStats, currentStats, stat)
+    ) {
+      return stat;
+    }
+    return current;
+  }, null)
+);
 
 export const levelOptions = [...Array(100).keys()].map(level => ({
   label: level + 1, value: level + 1
@@ -45,32 +70,6 @@ export const pokemonOptions = pokemon.reduce((accum, spec) => {
 }, []);
 
 export const algorithmOptions = [
-  {
-    label: "Random",
-    value: (originalStats, currentStats) => {
-      const stats = Object.keys(originalStats);
-      const nextStat = stats[Math.floor(Math.random() * stats.length)];
-
-      if (canRaiseStat(originalStats, currentStats, nextStat)) {
-        return nextStat;
-      }
-      return options.random(originalStats, currentStats);
-    }
-  },
-  {
-    label: "Even",
-    value: (originalStats, currentStats) => (
-      Object.keys(originalStats).reduce((current, stat) => {
-        const delta = currentStats[stat] - originalStats[stat];
-        if (
-          (current === null ||
-            delta < currentStats[current] - originalStats[current]) &&
-          canRaiseStat(originalStats, currentStats, stat)
-        ) {
-          return stat;
-        }
-        return current;
-      }, null)
-    )
-  }
+  { label: "Random", value: raiseRandomStat },
+  { label: "Even", value: raiseEvenStat }
 ];
