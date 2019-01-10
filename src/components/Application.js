88a82c00bb04da930c333e2306c76fd42542cc33
Kevin Deisz <kevin.deisz@gmail.com>
2019-01-10 10:58:38 -0500

    Inline selector

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
+import Select from "react-select";
 
-import Selector from "./Selector";
 import PokeDisplay from "./PokeDisplay";
 
 import { levelSelectorProps, natureSelectorProps, pokemonSelectorProps, algorithmSelectorProps } from "../lib/selector-props";
@@ -26,6 +26,35 @@ const Input = ({ children, name }) => (
     </Row>
   </>
 );
+
+class Selector extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { selected: null };
+    this.update = this.update.bind(this);
+  }
+
+  random() {
+    const { displays } = this.props;
+
+    this.update(displays[Math.floor(Math.random() * displays.length)]);
+  }
+
+  update(value) {
+    const { options, update } = this.props;
+
+    this.setState({ selected: value });
+    update(options[value ? value.value : null]);
+  }
+
+  render() {
+    const { displays } = this.props;
+    const { selected } = this.state;
+
+    return <Select options={displays} onChange={this.update} value={selected} className="selector" />;
+  }
+}
 
 class Application extends Component {
   constructor(props) {
