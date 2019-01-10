e5e006eeb56093d1db41485a1602a7973a9f30f8
Kevin Deisz <kevin.deisz@gmail.com>
2019-01-10 12:41:00 -0500

    Further simplify app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { useState, useCallback } from "react";
-import Select from "react-select";
+import ReactSelect from "react-select";
 
-import pokemon from "./pokemon";
-import natures from "./natures";
 import leveler from "./leveler";
+import natures from "./natures";
+import pokemon from "./pokemon";
 import "./styles";
 
-const pokemonOptions = pokemon.reduce((accum, spec) => {
-  if (!spec.Name) {
-    return accum;
-  }
-
-  const poke = {
+const makePokemonOpt = spec => {
+  const value = {
     number: spec.Number,
     name: spec.Name,
     hp: parseInt(spec.HP),
@@ -22,144 +18,150 @@ const pokemonOptions = pokemon.reduce((accum, spec) => {
     speed: parseInt(spec.Speed)
   };
 
-  let label = poke.name;
-  if (poke.number) {
-    label = `(#${poke.number}) ${poke.name}`;
+  let label = value.name;
+  if (value.number) {
+    label = `(#${value.number}) ${value.name}`;
   }
 
-  return [...accum, { label, value: poke }];
-}, []);
+  return { label, value };
+};
 
-const levelOptions = [...Array(100).keys()].map(level => ({
-  label: level + 1, value: level + 1
-}));
+const pokemonOpts = pokemon.filter(({ Name }) => Name).map(makePokemonOpt);
+const levelOpts = [...Array(100).keys()].map(level => ({ label: level + 1, value: level + 1 }));
+const natureOpts = natures.map(nature => ({ label: nature.Nature, value: nature }));
+const algorithmOpts = [{ label: "Random", value: "Random" }, { label: "Even", value: "Even" }];
 
-const natureOptions = natures.map(nature => ({
-  label: nature.Nature, value: nature
-}));
+const makeContainer = className => ({ children }) => <div className={className}>{children}</div>;
 
-const algorithmOptions = [
-  { label: "Random", value: "Random" },
-  { label: "Even", value: "Even" }
-];
+const Container = makeContainer("container");
+const Row = makeContainer("row");
+const Cols = makeContainer("col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3");
+const ButtonCols = makeContainer("col-xs-2 col-sm-offset-1 col-md-offset-2 col-lg-1 col-lg-offset-3");
+const RestCols = makeContainer("col-xs-9 col-xs-offset-1 col-sm-8 col-sm-offset-0 col-md-6 col-lg-5");
+const SplitCols = makeContainer("col-xs-6");
+const Well = makeContainer("well");
 
-const Row = ({ children }) => (
-  <div className="row">
-    <div className="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
-      {children}
-    </div>
-  </div>
+const Button = ({ children, onClick }) => (
+  <button type="button" className="btn btn-primary" onClick={onClick}>
+    {children}
+  </button>
 );
 
-const Input = ({ children, name }) => (
-  <>
-    <Row>
-      <strong>{name}</strong>
-    </Row>
-    <Row>
-      {children}
-    </Row>
-  </>
+const Label = ({ children }) => (
+  <Row>
+    <Cols>
+      <strong>{children}</strong>
+    </Cols>
+  </Row>
 );
 
-const PokeDisplay = ({ poke, level }) => (
-  <ul className="list-unstyled">
-    <li>{level + poke.hp * 3 + 10}</li>
-    <li>{poke.attack}</li>
-    <li>{poke.defense}</li>
-    <li>{poke.sAtk}</li>
-    <li>{poke.sDef}</li>
-    <li>{poke.speed}</li>
-  </ul>
-);
+const Select = props => <ReactSelect className="select" {...props} />;
 
-const Application = () => {
-  const [levelOption, setLevelOption] = useState(null);
-  const [natureOption, setNatureOption] = useState(null);
-  const [pokemonOption, setPokemonOption] = useState(null);
-  const [algorithmOption, setAlgorithmOption] = useState(null);
+const Text = ({ children }) => children;
 
-  const onRandomNatureClick = useCallback(event => {
-    event.preventDefault();
-    setNatureOption(natureOptions[Math.floor(Math.random() * natureOptions.length)]);
-  }, []);
+const getRandomNature = () => natureOpts[Math.floor(Math.random() * natureOpts.length)];
+
+const App = () => {
+  const [pokemonOpt, setPokemonOpt] = useState(pokemonOpts[0]);
+  const [levelOpt, setLevelOpt] = useState(levelOpts[0]);
+  const [natureOpt, setNatureOpt] = useState(getRandomNature());
+  const [algorithmOpt, setAlgorithmOpt] = useState(algorithmOpts[0]);
+
+  const onRandomNatureClick = useCallback(() => setNatureOpt(getRandomNature()), []);
+
+  const poke = leveler(levelOpt.value, natureOpt.value, pokemonOpt.value, algorithmOpt.value);
 
   return (
     <>
       <header>
-        PokeRPG Pokemon Builder
+        <Text>PokeRPG Pokemon Builder</Text>
         <a href="PokeRPG-Base-Stat-Info.pdf" className="pull-right">
-          <span className="glyphicon glyphicon-file" /> (PDF)
+          <span className="glyphicon glyphicon-file" />
+          {" "}
+          <Text>(PDF)</Text>
         </a>
       </header>
-      <div className="container">
-        <Input name="Pokemon">
-          <Select
-            className="select"
-            options={pokemonOptions}
-            value={pokemonOption}
-            onChange={setPokemonOption}
-          />
-        </Input>
-        <Input name="Level">
-          <Select
-            className="select"
-            options={levelOptions}
-            value={levelOption}
-            onChange={setLevelOption}
-          />
-        </Input>
+      <Container>
+        <Label>
+          <Text>Pokemon</Text>
+        </Label>
         <Row>
-          <strong>Nature</strong>
+          <Cols>
+            <Select options={pokemonOpts} value={pokemonOpt} onChange={setPokemonOpt} />
+          </Cols>
         </Row>
-        <div className="row">
-          <div className="col-xs-2 col-sm-offset-1 col-md-offset-2 col-lg-1 col-lg-offset-3">
-            <button type="button" className="btn btn-primary" onClick={onRandomNatureClick}>
-              Random
-            </button>
-          </div>
-          <div className="col-xs-9 col-xs-offset-1 col-sm-8 col-sm-offset-0 col-md-6 col-lg-5">
-            <Select
-              className="select"
-              options={natureOptions}
-              value={natureOption}
-              onChange={setNatureOption}
-            />
-          </div>
-        </div>
-        <Input name="Algorithm">
-          <Select
-            className="select"
-            options={algorithmOptions}
-            value={algorithmOption}
-            onChange={setAlgorithmOption}
-          />
-        </Input>
+        <Label>
+          <Text>Level</Text>
+        </Label>
         <Row>
-          {levelOption && natureOption && pokemonOption && algorithmOption && (  
-            <div className="well poke-display clearfix">
-              <div className="col-xs-6">
-                <ul className="list-unstyled text-right strong">
-                  <li>HP</li>
-                  <li>Attack</li>
-                  <li>Defense</li>
-                  <li>Special Attack</li>
-                  <li>Special Defense</li>
-                  <li>Speed</li>
-                </ul>
-              </div>
-              <div className="col-xs-6">
-                <PokeDisplay
-                  poke={leveler(levelOption.value, natureOption.value, pokemonOption.value, algorithmOption.value)}
-                  level={levelOption.value}
-                />
-              </div>
-            </div>
-          )}
+          <Cols>
+            <Select options={levelOpts} value={levelOpt} onChange={setLevelOpt} />
+          </Cols>
         </Row>
-      </div>
+        <Label>
+          <Text>Nature</Text>
+        </Label>
+        <Row>
+          <ButtonCols>
+            <Button onClick={onRandomNatureClick}>
+              <Text>Random</Text>
+            </Button>
+          </ButtonCols>
+          <RestCols>
+            <Select options={natureOpts} value={natureOpt} onChange={setNatureOpt} />
+          </RestCols>
+        </Row>
+        <Label>
+          <Text>Algorithm</Text>
+        </Label>
+        <Row>
+          <Cols>
+            <Select options={algorithmOpts} value={algorithmOpt} onChange={setAlgorithmOpt} />
+          </Cols>
+        </Row>
+        <Row>
+          <Cols>
+            <Well>
+              <Row>
+                <SplitCols>
+                  <ul className="list-unstyled text-right strong">
+                    <li>
+                      <Text>HP</Text>
+                    </li>
+                    <li>
+                      <Text>Attack</Text>
+                    </li>
+                    <li>
+                      <Text>Defense</Text>
+                    </li>
+                    <li>
+                      <Text>Special Attack</Text>
+                    </li>
+                    <li>
+                      <Text>Special Defense</Text>
+                    </li>
+                    <li>
+                      <Text>Speed</Text>
+                    </li>
+                  </ul>
+                </SplitCols>
+                <SplitCols>
+                  <ul className="list-unstyled">
+                    <li>{levelOpt.value + poke.hp * 3 + 10}</li>
+                    <li>{poke.attack}</li>
+                    <li>{poke.defense}</li>
+                    <li>{poke.sAtk}</li>
+                    <li>{poke.sDef}</li>
+                    <li>{poke.speed}</li>
+                  </ul>
+                </SplitCols>
+              </Row>
+            </Well>
+          </Cols>
+        </Row>
+      </Container>
     </>
   );
 };
 
-export default Application;
+export default App;
